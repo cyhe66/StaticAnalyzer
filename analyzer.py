1f875f4a07c728970541fa7695b307bd6218d0b8
Casey He <casey.he@gmail.com>
2018-11-28 19:53:23 -0500

    added unfinished comment-blocking code. line number matching needs to be fixed.

--- analyzer.py
+++ analyzer.py
@@ -1,4 +1,6 @@
+import logging
 import argparse
+import getopt
 import sys
 import re
 import c_ruleset
@@ -6,22 +8,56 @@ import c_ruleset
 def parse_args():
     parser = argparse.ArgumentParser(description='Process c source code.')
     parser.add_argument('infile', nargs='+', type=argparse.FileType('r'), default=sys.stdin)
-    parser.add_argument('-o', dest='outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+    #parser.add_argument('-o', dest='outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
     args = parser.parse_args()
-    return args.infile, args.outfile
+    return args.infile #args.outfile
 
-def analyze(infiles, outfile):
+def comment_remover(text):
+    def replacer(match):
+        s = match.group(0)
+        if s.startswith('/'):
+            return " " # note: a space and not an empty string
+        else:
+            return s
+    pattern = re.compile(r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"', re.DOTALL | re.MULTILINE)
+    return re.sub(pattern, replacer, text)
+
+def analyze(infiles):
     for infile in infiles:
-        i = 0
-        outfile.writelines("Analyzing " + infile.name + "...\n")
-        for line in infile:
-            i+=1
-            line = re.split(r'\W+', line)
-            for foo in line:
-                if foo in c_ruleset.c_ruleset:
-                    outfile.writelines("Line " + str(i) + ": " + foo + "\n")
-                    outfile.writelines(c_ruleset.c_ruleset[foo][2] + "\n")
+        code_tuple = [] 
+        code = infile.readlines()
+        length = 0
+        for line in code:
+            length += 1
+            #print(line + str(length))
+            code_tuple.append((line,length))
+        #print(code_tuple)
+
+        for line in code_tuple:
+            line  = (comment_remover(line[0]), line[1])
+            print(line)
+        '''
+        code = "".join(infile.readlines())
+        clean_code = comment_remover(code)
+        line_number = 0
+        for line in clean_code.split('\n'):
+            print(line)
+            line_number += 1
+            line = re.split(r'\W+',line)
+            for word in line:
+                if word in rules:
+                    logging.warning("Line " + str(line_number) + ": " + word + "\n")
+        '''
+    print('done with analysis.')
 
 if __name__ == "__main__":
-    infiles, outfile = parse_args()
-    analyze(infiles, outfile)
+    infiles = parse_args()
+    rules = c_ruleset.ruleset()
+    '''
+    for key in rules:
+        print(key)
+    '''
+    #infiles, outfile = parse_args()
+    #print(outfile.readall())
+    logging.basicConfig(level=logging.WARNING)
+    analyze(infiles)
--- c_ruleset.py
+++ c_ruleset.py
@@ -22,26 +22,26 @@ def ms_banned(hit):
     #Output to outfile/stderr:
     message = "This function is on the Microsoft 'banned list' due to known security flaws. See https://msdn.microsoft.com/en-us/library/bb288454.aspx for a suggested replacement"
     return
-
-c_ruleset = {
-    "strcpy":
-    (ms_banned, None, None, None, None, None),
-    "strcpyA|strcpyW|StrCpy|StrCpyA|lstrcpyA|lstrcpyW|_tccpy|_mbccpy|_ftcscpy|_mbsncpy|StrCpyN|StrCpyNA|StrCpyNW|StrNCpy|strcpynA|StrNCpyA|StrNCpyW|lstrcpynA|lstrcpynW":
-    (ms_banned, None, None, None, None, None),
-    "lstrcpy|wcscpy|_tcscpy|_mbscpy":
-    (ms_banned, None, None, None, None, None),
-    "strcat":
-    (ms_banned, None, None, None, None, None),
-    "lstrcat|wcscat|_tcscat|_mbscat":
-    (ms_banned, None, None, None, None, None),
-    "StrCat|StrCatA|StrcatW|lstrcatA|lstrcatW|strCatBuff|StrCatBuffA|StrCatBuffW|StrCatChainW|_tccat|_mbccat|_ftcsat|StrCatN|StrCatNA|StrCatNW|StrNCat|StrNCatA|StrNCatW|lstrncat|lstrcatnA|lstrcatnW":
-    (ms_banned, None, None, None, None, None),
-    "strncpy":
-    (ms_banned, None, None, None, None, None),
-    "lstrcpyn|wcsncpy|_tcsncpy|_mbsnbcpy":
-    (ms_banned, None, None, None, None, None),
-    "strncat":
-    (ms_banned, None, None, None, None, None),
-    "lstrcatn|wcsncat|_tcsncat|_mbsnbcat":
-    (ms_banned, None, None, None, None, None),
-}
+def ruleset():
+    return {
+        "strcpy":
+        (ms_banned, None, None, None, None, None),
+        "strcpyA|strcpyW|StrCpy|StrCpyA|lstrcpyA|lstrcpyW|_tccpy|_mbccpy|_ftcscpy|_mbsncpy|StrCpyN|StrCpyNA|StrCpyNW|StrNCpy|strcpynA|StrNCpyA|StrNCpyW|lstrcpynA|lstrcpynW":
+        (ms_banned, None, None, None, None, None),
+        "lstrcpy|wcscpy|_tcscpy|_mbscpy":
+        (ms_banned, None, None, None, None, None),
+        "strcat":
+        (ms_banned, None, None, None, None, None),
+        "lstrcat|wcscat|_tcscat|_mbscat":
+        (ms_banned, None, None, None, None, None),
+        "StrCat|StrCatA|StrcatW|lstrcatA|lstrcatW|strCatBuff|StrCatBuffA|StrCatBuffW|StrCatChainW|_tccat|_mbccat|_ftcsat|StrCatN|StrCatNA|StrCatNW|StrNCat|StrNCatA|StrNCatW|lstrncat|lstrcatnA|lstrcatnW":
+        (ms_banned, None, None, None, None, None),
+        "strncpy":
+        (ms_banned, None, None, None, None, None),
+        "lstrcpyn|wcsncpy|_tcsncpy|_mbsnbcpy":
+        (ms_banned, None, None, None, None, None),
+        "strncat":
+        (ms_banned, None, None, None, None, None),
+        "lstrcatn|wcsncat|_tcsncat|_mbsnbcat":
+        (ms_banned, None, None, None, None, None),
+    }
