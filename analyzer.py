148546b6cfbd68510413bec7fbfca3f1b0847d3d
Casey He <casey.he@gmail.com>
2018-12-14 18:25:21 -0500

    temp commit

--- analyzer.py
+++ analyzer.py
@@ -85,7 +85,6 @@ def analyze(infiles, rules):
                     'default', 'enum', 'goto', 'register', 'sizeof', 
                     'typedef', 'volatile', 'char', 'do', 'extern', 'if', 
                     'return', 'static', 'union', 'while']
-
     for line in clean_code:
         d = declaration.match(line[0])
         if d:
@@ -119,40 +118,22 @@ def analyze(infiles, rules):
             #print(map(str.strip, arguments))
             argts = [item.strip() for item in arguments]
             #print(argts)
-            if f.group('funct') not in c_keywords:
+            #if f.group('funct') not in c_keywords:
                 #create a key entry for the function function_line#_char#
-                key = f.group('funct')+"_"+str(line[1])+"_"+str(line[0].find(f.group('funct')))
-                function_dict[key] = argts
-                #print(key)
-                #print(line[1],'var:',f.group('var'), '| function:',f.group('funct'), '| argts:',argts)
-            print(function_dict) 
-        '''
-        code = re.split(r'\W+', line[0])
-        for word in code:
-            if word in rules:
-                logging.warning('line '+str(line_number)+': '+word+' used.')
-        '''
-        """
-            if m = assignment.match(word):
-                type = m.group(1)
-                name = word after type (if not a declaration/initialization, skip)
-                if name followed by [ (array/buffer):
-                    size = number after [ (if variable, lookup value)
-                if next is =:
-                    value = number after '='
-                elif next is ;:
-                    value = 0
-                vars[name] = (type, value, False, size?)
-        """
-    print(var_dict)
-    return clean_code
+            key = f.group('funct')+"_"+str(line[1])+"_"+str(line[0].find(f.group('funct')))
+            function_dict[key] = argts
+            #print(function_dict) 
+    #print(var_dict)
+    return clean_code, var_dict, function_dict
 
 # returns the line number of the mmap and munmap, as well as the variable that
 # is associated
-def word_scope(function_name, exit_name, start_line, end_line, code):
+def word_scope(function_name, exit_name, start_line, end_line, code, funct_dict):
     stack = []
     #end_line = 75
-    segment = code[start_line:end_line]
+    segment = code[start_line:end_line-1]#save special check for the last line
+    close_re = r".+\W" + exit_name + r"\W.+"
+    last_line = 
     open_brackets = "{("
     close_brackets = "})"
     for tup in segment:
@@ -164,16 +145,28 @@ def word_scope(function_name, exit_name, start_line, end_line, code):
                     stack.pop()
                 if letter == ")" and stack[-1] == "(":
                     stack.pop()
-        close_re = r".+\W" + exit_name + r"\W.+"
         #but this could just be any instance of munmap
+
+    #if the call to unmap is normal, the stack structure shoud be ok
+    #if the call to unmap is in an if statement, we have to strip the if statement to only use the argument
+        #this can be done by checking the function dictionary and the lines for which are called
+    #print(code[75][0])
     if re.match(close_re, tup[0]) != None:  # found an instance of munmap 
+        #tup[0] is the line with the closing operator 
+        print(tup[0])
+        if (len(stack) == 0):
+            print('empty stack. We gucci.')
+        print(stack)
+
+
+        '''
         if len(stack) == 0:
             print(tup[1])
         else:
             print('Stack unresolved: ', stack,' at line :', tup[1])
             print(tup[0])
             print(tup[0].find('munmap'))
-
+        '''
 if __name__ == "__main__":
     infiles = parse_args()
     rules = c_ruleset.ruleset()
@@ -181,9 +174,9 @@ if __name__ == "__main__":
     #print(outfile.readall())
     logging.basicConfig(level=logging.WARNING)
     logging.warning('started analysis')
-    clean_code = analyze(infiles, rules)
+    clean_code, var_dict, funct_dict = analyze(infiles, rules)
     logging.warning('done with analysis.')
     #pair_finder(clean_code)
-    word_scope('mmap','munmap',60,75,clean_code)
+    word_scope('mmap','munmap',60,75,clean_code, funct_dict)
     #print (clean_code)
 
