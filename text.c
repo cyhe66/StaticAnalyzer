d77ef333cc2a8d0d57dc2d508c5c138600a28044
ceegeechow <camille@chowww.com>
2018-12-19 13:55:23 -0500

    cleaned up comments, debugged text.c so it passes compiler

--- text.c
+++ text.c
@@ -1,9 +1,13 @@
+//NOTE: This file compiles with no errors or warnings
+
+#include <stdio.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <assert.h>
+#include <strings.h> 
 size_t getFilesize(const char* filename) {
 	struct stat st;
 	stat(filename, &st);
@@ -14,19 +18,20 @@ size_t getFilesize(const char* filename) {
 /* multiline
 comment
 */
-int j = 0;/*and another one-
-right here*/ int k;
-int a[j];
-int l;
-int x;
 
 int main(int argc, char** argv) {
-	//size_t filesize = getFilesize(argv[1]);
+	int j = 0;/*and another one-
+	right here*/ int k;
+	int a[j];
+	void* l;
+	int x;
+
+	size_t filesize = getFilesize(argv[1]);
 	//Open file
 	int fd = open(argv[1], O_RDONLY, 0);
 	assert(fd != -1);
 	//Execute mmap
-	void* mmappedData[10] = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE | MAP_POPULATE | MAP_FIXED, fd, 11);
+	char* mmappedData = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE | MAP_FIXED, fd, 11);
 	assert(mmappedData != MAP_FAILED);
 	//Write the mmapped data to stdout (= FD #1)
 	write(1, mmappedData, filesize);
@@ -35,10 +40,9 @@ int main(int argc, char** argv) {
 	assert(rc == 0);
 	close(fd);
 	void* c = mmap(NULL, 32, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANON, -1, 0);
-	strcpy();
-	bzero();
-	k = l;
-	memcpy();
+	strcpy(c, "hello world");
+	bzero(l,x);
 	setuid(1);
-	snprintf(buf, 128, argv[1]);
+	char* buf;
+	printf(buf, 128, argv[1]);
 }
